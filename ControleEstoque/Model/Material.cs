bfb97b5b9c80e5b8462367164444e5ed2f7c018a
João Victor <joaovictormarineli@gmail.com>
2018-06-12 22:34:19 -0300

    Criando model de materiais, geters e seters

---
+++ ControleEstoque/Model/Material.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Material
+    {
+        public int MaterialId { get; set; }
+
+        public string MaterialNome { get; set; }
+
+        public string MaterialDesc { get; set; }
+    }
+}
