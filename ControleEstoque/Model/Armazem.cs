70deee2d9ca544dae5153190e7519f712fdd7088
João Victor <joaovictormarineli@gmail.com>
2018-06-12 23:32:48 -0300

    Model funcionario e armazem

---
+++ ControleEstoque/Model/Armazem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    class Armazem
+    {
+        public int ArmazemId { get; set; }
+
+        public string Local { get; set; }
+
+        public string ArmazemDesc { get; set; }
+    }
+}
---
+++ ControleEstoque/Model/Funcionario.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    class Funcionario
+    {
+        public int FuncionarioId { get; set; }
+
+        public string NomeFuncionario { get; set; }
+
+        public string Cargo { get; set; }
+    }
+}
