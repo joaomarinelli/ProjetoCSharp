551b8fc48ab2af3953dfeb3cc1e80a49ccf7cb4b
TiranoMax <alexsandroarents@hotmail.com>
2018-06-27 23:47:02 -0300

    Arumado erro de Usings na classe EditFuncionario

--- ControleEstoque/ControleEstoque/EditFuncionario.xaml.cs
+++ ControleEstoque/ControleEstoque/EditFuncionario.xaml.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Controllers;
+using Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting.Contexts;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
